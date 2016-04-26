74e68ff2077649b3f763210fbe8b5256ec7b08fb
Evan Watterud <cod.videos@hotmail.com>
2016-04-26 00:26:00 -0400

    answering-questions feature patch

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
-    @answers = Answer.all
+    @answers = @question.answers
     @answer = Answer.new
   end
 
